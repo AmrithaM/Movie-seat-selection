3bb32163a291d3ad4ce3953d9692e0febaf2c955
Amritha Marimuthu <amritha@Amrithas-MacBook-Air.local>
2020-03-30 12:44:16 -0400

    Save to localstorage

--- script.js
+++ script.js
@@ -5,17 +5,50 @@ const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 let ticketPrice = +movieSelect.value;
 
+populateUI();
+
 //update total and count
-const updateSelectedCount = () => {
+function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll(".row .seat.selected");
     const selectedSeatsCount = selectedSeats.length;
+    
+    const seatsIndex = [...selectedSeats].map(seat=> [...seats].indexOf(seat));
+    localStorage.setItem("seatsIndex", JSON.stringify(seatsIndex));
+
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount*ticketPrice;
 }
 
+//Get data from localstorage and poulate UI
+function populateUI(){
+    const selectedSeats = JSON.parse(localStorage.getItem("seatsIndex"));
+    
+    if(selectedSeats !== null && selectedSeats.length > 0){
+
+        seats.forEach((seat, index)=>{
+            if(selectedSeats.indexOf(index) > -1){
+                seat.classList.add("selected");
+            }
+        })
+
+    }
+
+    //populate the select dropdown
+    const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+    if(selectedMovieIndex !== null){
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
 //Movie select dropdown
 movieSelect.addEventListener("change", e => {
+    
     ticketPrice = +e.target.value;
+
+    //save movie details to the local storage
+    localStorage.setItem("selectedMovieIndex", e.target.selectedIndex);
+
     updateSelectedCount();
 })
 
@@ -27,3 +60,5 @@ container.addEventListener("click", e => {
         updateSelectedCount();
     }
 })
+
+updateSelectedCount();
